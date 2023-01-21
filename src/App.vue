3fd2fb6a6890e3dc245559202d89f06aad79d239
MissUsagi <syljanko@gmail.com>
2023-01-21 18:23:24 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<data-table :columns="tableColumns" :rows="dataRows"></data-table>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import DataTable from './components/DataTable.vue';
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components: { DataTable },
+    data(){
+  return {
+    tableColumns: [{accessor: 'name', label: "Name"}, 
+    {accessor: 'age', label: "Age"}, 
+    {accessor: 'is_Manager', label: "Manager"}, 
+    {accessor: 'start_date', label: "Start Date"}],
+    dataRows: [
+  { id: 1, name: 'Liz Lemon', age: 36, is_manager: true, start_date: '02-28-1999' },
+  { id: 2, name: 'Jack Donaghy', age: 40, is_manager: true, start_date: '03-05-1997' },
+  { id: 3, name: 'Tracy Morgan', age: 39, is_manager: false, start_date: '07-12-2002' },
+  { id: 4, name: 'Jenna Maroney', age: 40, is_manager: false, start_date: '02-28-1999' },
+  { id: 5, name: 'Kenneth Parcell', age: Infinity, is_manager: false, start_date: '01-01-1970' },
+  { id: 6, name: 'Pete Hornberger', age: null, is_manager: true, start_date: '04-01-2000' },
+  { id: 7, name: 'Frank Rossitano', age: 36, is_manager: false, start_date: null },
+  { id: 8, name: null, age: null, is_manager: null, start_date: null },
+],
   }
+}
 }
 </script>
 
---
+++ src/components/DataTable.vue
@@ -0,0 +1,60 @@
+<template>
+   <div class="container">
+   <table>
+      <thead @click="filter">
+         <tr>
+            <th v-for="column in columns" :key="column.accessor">{{ column.label }}</th>
+         </tr>
+      </thead>
+      <tbody>
+         <tr v-for="row in rows" :key="row.id">
+            <td>{{ row.name }}</td>
+            <td>{{ row.age }}</td>
+            <td>{{ row.is_manager }}</td>
+            <td>{{ row.start_date }}</td>
+         </tr>
+      </tbody>
+   </table>
+   </div>
+</template>
+
+<script>
+  export default {
+   props: ['columns', 'rows'],
+//    data(){
+//    return{
+//       filteredRows: this.rows,
+//    }
+//   },
+  }
+
+</script>
+
+<style scoped>
+.container {
+	margin-right:auto;
+	margin-left:auto;
+	display:flex;
+	justify-content:center;
+}
+table th {
+  text-transform: uppercase;
+  text-align: left;
+  background: #44475C;
+  color: #FFF;
+  padding: 8px;
+  min-width: 50px;
+}
+
+table td {
+min-width: 250px;
+  text-align: left;
+  padding: 8px;
+  /* border-right: 2px solid #7D82A8; */
+}
+table td:last-child {
+  border-right: none;
+}
+table tbody tr:nth-child(2n) td {
+  background: #D4D8F9;}
+</style>
