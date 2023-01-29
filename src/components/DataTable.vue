4f3ac8094e77f08823bc86beae1ac9e0cf59fbb1
MissUsagi <syljanko@gmail.com>
2023-01-29 18:49:48 +0100

    basic sort by name and custom table size

--- src/components/DataTable.vue
+++ src/components/DataTable.vue
@@ -1,118 +1,153 @@
 <template>
    <div class="container">
-   <table>
-      <thead @click="filter">
-         <tr>
-            <th v-for="column in columns" :key="column.accessor">{{ column.label }}</th>
-         </tr>
-      </thead>
-      <tbody>
-         <tr>
-            <td class="input-row"><input type="type" placeholder="Search Name" @input="searchName($event)">
-               <!-- <span @click="sortData">Sortuj</span> -->
-            </td>
-         </tr>
-         <tr v-for="row in visibleData" :key="row.id">
-            <td>{{ row.name }}</td>
-            <td>{{ row.age }}</td>
-            <td><span>{{ row.is_manager ? '✔️' : '✖️' }}</span> {{ row.is_manager }}</td>
-            <td>{{ row.start_date }}</td>
-         </tr>
-      </tbody>
-   </table>
-   <pagination-component :current-Page="currentPage" :pages="pagesInTotal" @my-event="changePage"></pagination-component>
+      <table>
+         <thead @click="filter">
+            <tr>
+               <th v-for="column in columns" :key="column.accessor">{{ column.label }} <span
+                     @click="sortData">Sort</span></th>
+            </tr>
+         </thead>
+         <tbody>
+            <tr>
+               <td class="input-row"><input type="type" placeholder="Search Name" @input="searchName($event)">
+                  <!-- <span @click="sortData">Sortuj</span> -->
+               </td>
+            </tr>
+            <tr v-for="row in visibleData" :key="row.id">
+               <td>{{ row.name }}</td>
+               <td>{{ row.age }}</td>
+               <td><span>{{ row.is_manager ? '✔️' : '✖️' }}</span> {{ row.is_manager }}</td>
+               <td>{{ row.start_date }}</td>
+            </tr>
+         </tbody>
+      </table>
+      <pagination-component :current-Page="currentPage" :pages="pagesInTotal" @my-event="changePage"
+         @table-size="updatePage"></pagination-component>
    </div>
 </template>
 
 <script>
 import PaginationComponent from './PaginationComponent.vue';
-  export default {
-   components: {PaginationComponent},
+export default {
+   components: { PaginationComponent },
    props: ['columns', 'rows'],
-   data(){
-   return{
-      pageSize: 3,
-      currentPage: 0,
-      visibleData: [],
-      pagesInTotal: null,
+   data() {
+      return {
+         data: this.rows,
+         pageSize: 3,
+         currentPage: 0,
+         visibleData: [],
+         pagesInTotal: null,
+      }
+   },
+   mounted: function () {
+      this.updateVisibleData()
+   },
+   methods: {
+      updateVisibleData() {
+         this.pagesInTotal = Math.ceil(this.data.length / this.pageSize);
+         this.visibleData = this.data.slice(this.currentPage * this.pageSize, (this.currentPage * this.pageSize) + this.pageSize)
+         console.log(this.currentPage)
+      },
+      searchName(e) {
+         const searchedPerson = e.target.value;
+         console.log(searchedPerson)
+         if (e.target.value) {
+            this.visibleData = this.data.filter(person => {
+               if (person.name.toLowerCase().includes(searchedPerson.toLowerCase()))
+                  return person
+            });
+         }
+         else this.updateVisibleData();
+
+      },
+
+      changePage(page) {
+         const pageIndex = page;
+         this.currentPage = pageIndex;
+         this.updateVisibleData();
+      },
+      updatePage(size) {
+         this.pageSize = size;
+         this.currentPage = 0;
+         this.updateVisibleData();
+      },
+      sortData() {
+         // const sortedTable = [...this.data];
+         // console.log(sortedTable)
+         this.data.sort((a, b) => {
+            const nameA = a.name.toUpperCase();
+            const nameB = b.name.toUpperCase();
+            if (nameA < nameB) {
+               return -1;
+            }
+            if (nameA > nameB) {
+               return 1;
+            }
+            return 0;
+         });
+         this.updateVisibleData();
+      }
    }
-  },
-  mounted: function(){
-   this.updateVisibleData()
-  },
-  methods: {
-   updateVisibleData(){
-      this.pagesInTotal = Math.ceil(this.rows.length / this.pageSize);
-      this.visibleData = this.rows.slice(this.currentPage * this.pageSize, (this.currentPage * this.pageSize) + this.pageSize)
-      console.log(this.currentPage)
-   },
-   searchName(e){
-      const searchedPerson = e.target.value;
-      console.log(searchedPerson)
-      if(e.target.value){   
-         this.visibleData = this.rows.filter(person => {
-         if(person.name.toLowerCase().includes(searchedPerson.toLowerCase()))
-         return person
-      });}
-      else this.updateVisibleData();
-
-   },
-
-   changePage(page){
-      const pageIndex = page;
-      this.currentPage = pageIndex;
-      this.updateVisibleData(); 
-   },
-   // sortData(){
-   //    this.visibleData.sort((a,b) => b.id - a.id)
-   //    console.log(this.visibleData)
-   // }
-  }
-  }
+}
 
 </script>
 
 <style scoped>
 .container {
-	margin-right:auto;
-	margin-left:auto;
-	display:flex;
+   margin-right: auto;
+   margin-left: auto;
+   display: flex;
    flex-direction: column;
-	justify-content:center;
+   justify-content: center;
    border: 1px solid #333;
 }
+
 table th {
-  text-transform: uppercase;
-  text-align: left;
-  background: #44475C;
-  color: #FFF;
-  padding: 8px;
-  min-width: 50px;
+   position: relative;
+   text-transform: uppercase;
+   text-align: left;
+   background: #44475C;
+   color: #FFF;
+   padding: 8px;
+   min-width: 50px;
 }
 
 table td {
-min-width: 250px;
-  text-align: left;
-  padding: 8px;
-  /* border-right: 2px solid #7D82A8; */
+   min-width: 250px;
+   text-align: left;
+   padding: 8px;
+   /* border-right: 2px solid #7D82A8; */
 }
-table td:last-child {
-  border-right: none;
-}
-table tbody tr:nth-child(2n) td {
-  background: #D4D8F9;}
 
-  .input-row{
+table td:last-child {
+   border-right: none;
+}
+
+table tbody tr:nth-child(2n) td {
+   background: #D4D8F9;
+}
+
+span {
+   /* position: absolute; */
+   cursor: pointer;
+   right: 0;
+   top: 3px;
+}
+
+.input-row {
    display: flex;
    align-items: flex-start;
    justify-content: space-between;
-  }
+}
 
-.input-row span{
+.input-row span {
    font-weight: bold;
    cursor: pointer;
- }
- .input-row input, span {
+}
+
+.input-row input,
+span {
    padding: 5px;
- }
+}
 </style>
--- src/components/PaginationComponent.vue
+++ src/components/PaginationComponent.vue
@@ -1,65 +1,98 @@
 <template>
+   <div class="list">
    <ul>
       <li @click="goToPrevPage()">[Previous]</li>
-      <li v-for="(page, index) in pages" :key="page" @click="goToPage(index)" :class="{active: activePage === index}">[{{ page }}]</li>
+      <li v-for="(page, index) in pages" :key="page" @click="goToPage(index)" :class="{ active: activePage === index }">
+         [{{ page }}]</li>
       <li @click="goToNextPage()">[Next]</li>
    </ul>
+</div>
+   <div class="page-size">
+      <p>Table size</p>
+      <select id="page_size" name="page_size" v-model="tableSize">
+         <option value="3">3</option>
+         <option value="6">6</option>
+         <option value="9">9</option>
+         <option value="12">12</option>
+         <option value="15">15</option>
+      </select>
+      <button type="submit" @click="changeTableSize">Update</button>
+   </div>
 </template>
 
 <script>
 
-export default{
+export default {
    props: ['currentPage', 'pages'],
-   emits: ['my-event'],
-   data(){
+   emits: ['my-event', 'table-size'],
+   data() {
       return {
-         activePage: this.currentPage, 
+         activePage: this.currentPage,
+         tableSize: 3,
       }
    },
    methods: {
-      goToPage(page){
+      goToPage(page) {
          this.activePage = page;
-         this.$emit("my-event", this.activePage);      
+         this.$emit("my-event", this.activePage);
          console.log("page " + page)
          console.log("currentPage " + this.currentPage)
          console.log("activePage " + this.activePage)
-    },
-      goToNextPage(){
+      },
+      goToNextPage() {
          const nextPage = this.currentPage + 1; //current Page i active Page? ujednolicenie?
-         if(nextPage < this.pages){
+         if (nextPage < this.pages) {
             this.$emit("my-event", nextPage);
-            this.activePage +=1;
+            this.activePage += 1;
          }
 
       },
-      goToPrevPage(){
+      goToPrevPage() {
          const prevPage = this.currentPage - 1;
-         if(prevPage >= 0)
-         {
+         if (prevPage >= 0) {
             this.$emit("my-event", prevPage);
-            this.activePage -=1;
+            this.activePage -= 1;
          }
+      },
+      changeTableSize() {
+         console.log(this.tableSize);
+         this.$emit("table-size", this.tableSize);
+         this.activePage = 0;
       }
    }
 }
 </script>
 
 <style scoped>
+div .list {
+   display: flex;
+}
 ul {
    margin: auto;
    padding: 0.7rem 0;
    display: flex;
    flex-direction: row;
 }
+
 li {
    cursor: pointer;
    font-size: 20px;
    list-style: none;
    padding: 0 5px;
 }
+
 li:hover {
    color: purple;
    scale: 1.1;
+}
+
+button {
+   background: purple;
+   color: white;
+   padding: 5px 10px;
+   font-size: 1.2rem;
+   border: none;
+   border-radius: 0;
 }
 
 .active {
@@ -67,5 +100,21 @@ li:hover {
    color: purple
 }
 
+.page-size {
+   display: flex;
+   flex-direction: row;
+   gap: 10px;
+   align-items: center;
+   justify-content: center;
+   
+}
+
+.page-size p {
+   font-size: 1.2rem;
+}
+
+.page-size select {
+   padding: 5px 10px;
+}
 </style>
 
