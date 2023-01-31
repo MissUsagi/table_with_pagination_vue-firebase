8ef624cbadbb0db273b479338a4d943947c1ef34
MissUsagi <syljanko@gmail.com>
2023-01-31 20:49:20 +0100

    sort by date

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,7 @@ export default {
           { id: 8, name: null, age: null, is_manager: null, start_date: null },
           { id: 9, name: 'Beatrix Doe', age: 29, is_manager: true, start_date: '04-05-2020' },
           { id: 10, name: 'Sam Smith', age: 28, is_manager: false, start_date: '04-11-2022' },
-          { id: 11, name: 'Jill Jones', age: 31, is_manager: null, start_date: '24-08-2017' },
+          { id: 11, name: 'Jill Jones', age: 31, is_manager: null, start_date: '08-24-2017' },
           { id: 12, name: 'Pete Doe', age: 35, is_manager: false, start_date: '12-03-2019' },
           { id: 13, name: 'Kat More', age: 27, is_manager: false, start_date: '12-06-2022' },
           { id: 14, name: 'John Doe', age: 25, is_manager: true, start_date: '03-10-2018' },
